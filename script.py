286ddf3a613c2a3139867bdc63d991fc7557dc1c
Luke Koenigsknecht <lkoenigs@nd.edu>
2022-04-05 15:11:02 -0400

    csv update

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 raw_data = pd.read_csv('pppub21.csv')
-data = raw_data[['A_MARITL','A_SEX','A_HGA','A_AGE','PEAFEVER','PEARNVAL']]
-data.to_csv('data.csv')
+data = raw_data[['A_MARITL','A_SEX','A_HGA','A_AGE','PEAFEVER','WSAL_VAL']]
+data.to_csv('data.csv', index=False)
